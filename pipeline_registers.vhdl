ec6494904dddc2b93fa3da26a9fd0c717928abf0
georgeyork1 <george.york@afacademy.af.edu>
2025-06-23 15:27:23 -0600

    fix PC + 4

--- pipeline_registers.vhdl
+++ pipeline_registers.vhdl
@@ -16,7 +16,7 @@ entity pipeline_registers is
         if_id_jump : in STD_LOGIC;
         if_id_load_addr : in STD_LOGIC;
         if_id_instr : in  STD_LOGIC_VECTOR(31 downto 0);
-        if_id_pc    : in  STD_LOGIC_VECTOR(31 downto 0);
+        if_id_npc    : in  STD_LOGIC_VECTOR(31 downto 0);
         if_id_rd    : in STD_LOGIC_VECTOR(4 downto 0);
         if_id_reg1_data  : in  STD_LOGIC_VECTOR(31 downto 0);
         if_id_reg2_data  : in  STD_LOGIC_VECTOR(31 downto 0);
@@ -33,7 +33,7 @@ entity pipeline_registers is
         id_ex_load_addr : inout STD_LOGIC;
         id_ex_rd    : inout STD_LOGIC_VECTOR(4 downto 0);
         id_ex_instr : out STD_LOGIC_VECTOR(31 downto 0);
-        id_ex_pc    : inout STD_LOGIC_VECTOR(31 downto 0);
+        id_ex_npc    : inout STD_LOGIC_VECTOR(31 downto 0);
         id_ex_reg1_data  : inout  STD_LOGIC_VECTOR(31 downto 0);
         id_ex_reg2_data  : inout  STD_LOGIC_VECTOR(31 downto 0);
         id_ex_imm   : inout  STD_LOGIC_VECTOR(31 downto 0);
@@ -47,7 +47,7 @@ entity pipeline_registers is
         ex_mem_branch : out STD_LOGIC;
         ex_mem_jump : out STD_LOGIC;
         ex_mem_load_addr : inout STD_LOGIC;
-        ex_mem_pc    : out STD_LOGIC_VECTOR(31 downto 0);
+        ex_mem_npc    : out STD_LOGIC_VECTOR(31 downto 0);
         ex_mem_rd   : inout STD_LOGIC_VECTOR(4 downto 0);
         ex_mem_reg1_data : out STD_LOGIC_VECTOR(31 downto 0);
         ex_mem_reg2_data : out STD_LOGIC_VECTOR(31 downto 0);
@@ -78,7 +78,7 @@ begin
             id_ex_jump <= '0';
             id_ex_load_addr <= '0';
             id_ex_instr <= (others => '0');
-            id_ex_pc    <= (others => '0');
+            id_ex_npc    <= (others => '0');
             id_ex_rd   <= (others => '0');
             id_ex_reg1_data <= (others => '0');
             id_ex_reg2_data <= (others => '0');
@@ -92,7 +92,7 @@ begin
             ex_mem_branch <= '0';
             ex_mem_jump <= '0';
             ex_mem_load_addr <= '0';
-            ex_mem_pc <= (others => '0');
+            ex_mem_npc <= (others => '0');
             ex_mem_rd   <= (others => '0');
             ex_mem_reg1_data <= (others => '0');
             ex_mem_reg2_data <= (others => '0');
@@ -117,7 +117,7 @@ begin
             id_ex_load_addr <= if_id_load_addr; 
             id_ex_rd    <= if_id_rd;      
             id_ex_instr <= if_id_instr;
-            id_ex_pc    <= if_id_pc;
+            id_ex_npc    <= if_id_npc;
             id_ex_reg1_data  <= if_id_reg1_data;
             id_ex_reg2_data  <= if_id_reg2_data;
             id_ex_imm  <= if_id_imm;
@@ -130,7 +130,7 @@ begin
             ex_mem_branch <= id_ex_branch;
             ex_mem_jump <= id_ex_jump;
             ex_mem_load_addr <= id_ex_load_addr; 
-            ex_mem_pc <= id_ex_pc;
+            ex_mem_npc <= id_ex_npc;
             ex_mem_rd   <= id_ex_rd;      
             ex_mem_reg1_data <= id_ex_reg1_data;
             ex_mem_reg2_data <= id_ex_reg2_data;
